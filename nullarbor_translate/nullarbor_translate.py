993a23ad18cd454ae07630169f40cb58dc4fd48a
Anders Goncalves da Silva <andersgs@gmail.com>
2016-09-22 09:03:50 +1000

    Small typo in main script

--- nullarbor_translate/nullarbor_translate.py
+++ nullarbor_translate/nullarbor_translate.py
@@ -40,7 +40,7 @@ def main(template_file, config_file, nullarbor_report, job_id):
     nullarbor.add_mlst(config.filenames['mlst'])
     nullarbor.add_resistome(config.filenames['resistome'])
     nullarbor.add_newick(config.filenames['newick'])
-    nullabor.add_core_aln(config.filenames['core_aln'])
+    nullarbor.add_core_aln(config.filenames['core_aln'])
     nullarbor.add_sops(config.sops)
     nullarbor.render_template(template)
 
